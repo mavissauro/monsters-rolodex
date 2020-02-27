b0dc63537c32843f77aaf0cd79847f7b629583d0
Marcos <mvinicius.ribeiro68@gmail.com>
2020-02-26 21:51:56 -0300

    Updates

---
+++ precache-manifest.43ccfb3fb209737e071b12d348ffd03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3398a4b0b9bb493434ded1d7f25ff51f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26d7c8f2f32746937e12",
+    "url": "/monsters-rolodex/static/css/main.afaf1460.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d7c8f2f32746937e12",
+    "url": "/monsters-rolodex/static/js/main.f7e5b74d.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
